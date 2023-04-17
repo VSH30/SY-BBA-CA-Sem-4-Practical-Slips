d52aa09ebfd73b5a637ed0761290f286f1e0c0f2
Vishal Hanchnoli <hanchnoli@gmail.com>
2023-04-17 19:12:52 +0530

    Add files via upload

---
+++ SEM 4 SLIPS/C++/slip 30/30b.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+#include<iomanip>
+
+using namespace std;
+
+class Marksheet{
+    private:
+        int seat;
+    public:
+        void accept()
+};
+
+int main(){
+    Marksheet stud;
+    stud.getresult();
+    return 0;
+}
